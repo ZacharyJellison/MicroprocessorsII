7aba39bf4073d35f12bde84732a67fa003bcc5fa
ZacharyJellison <146299159+ZacharyJellison@users.noreply.github.com>
2024-02-08 15:00:43 -0500

    Added Project 1 code

---
+++ project1.ino
@@ -0,0 +1,72 @@
+//Variable Declerations
+boolean toggle = 0;
+boolean safety = 0;
+
+const int greenArrow = 10;
+const int green = 11;
+const int yellow = 12;
+const int red = 13;
+const int button = 2;
+const int buzzer = 8;
+
+
+void setup() {
+  pinMode(red, OUTPUT);
+  pinMode(yellow, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(greenArrow, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+
+  pinMode(button, INPUT);
+}
+
+//Needed for Interrupt function to work
+void safetyChange(){
+  safety = 1;
+}
+
+//Buzzer Sound and blink
+void buzzBlink(int flash){
+  digitalWrite(buzzer, HIGH);
+  digitalWrite(flash, LOW);
+  delay(1000);
+  digitalWrite(flash, HIGH);
+  delay(1000);
+  digitalWrite(flash, LOW);
+  delay(1000);
+  digitalWrite(buzzer, LOW);
+}
+
+void loop() {
+  toggle = digitalRead(button);         //Set Button read to toggle
+
+//Blinking red waiting for Button Press to Begin
+  while(safety == 0){
+  //Interrupt
+    attachInterrupt(digitalPinToInterrupt(button), safetyChange, RISING);
+
+  //Blink
+    digitalWrite(red, HIGH);
+    delay(1000);
+    digitalWrite(red, LOW);
+    delay(1000); 
+  }
+
+//Signal Change Loop
+  //Green Arrow
+    digitalWrite(greenArrow, HIGH);
+    delay(2000);
+    buzzBlink(greenArrow);
+  //Green Signal
+    digitalWrite(green, HIGH);
+    delay(9000);
+    buzzBlink(green);
+  //Yellow Signal
+    digitalWrite(yellow, HIGH);
+    delay(3000);
+    digitalWrite(yellow, LOW);
+  //Red Signal
+    digitalWrite(red, HIGH);
+    delay(17000);
+    buzzBlink(red);
+}
